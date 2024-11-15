5f841d00464e4aac688078428560c5519ba4f0bc
AvrilTraverso <avril.traverso9@gmail.com>
2024-11-15 18:44:44 -0300

    add class gestora usuarios

---
+++ src/ClasesGestoras/Usuarios.java
@@ -0,0 +1,35 @@
+package ClasesGestoras;
+import Clases.Usuario;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Usuarios {
+    Set<Usuario> listaUsuarios;
+
+    public Usuarios() {
+        this.listaUsuarios = new HashSet<>();
+    }
+
+    public void registrarUsuario(){
+
+    }
+
+    public String loguearUsuario(){
+        return null;
+    }
+
+    public String eliminarUsuario(){
+        return null;
+    }
+
+    public String mostrarUsuarios(){
+        return null;
+    }
+
+    public String filtrarPorClase(){
+        return null;
+    }
+}
+
+
+
