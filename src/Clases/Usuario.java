0afab6dcc82410d96e805df5b4a22990c9e76f56
Alejandro Agra <aleagra08@hotmail.com>
2024-11-15 12:49:25 -0300

    Create Class User

---
+++ src/Clases/Usuario.java
@@ -0,0 +1,94 @@
+package Clases;
+import java.util.Objects;
+
+public abstract class Usuario {
+    private static int contador=1;
+    public int id;
+    public String nombre;
+    public String apellido;
+    public String email;
+    public String contrasenia;
+
+    public Usuario(String nombre, String apellido, String email, String contrasenia) {
+        this.id = contador++;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.contrasenia = contrasenia;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public static void setContador(int contador) {
+        Usuario.contador = contador;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", contrasenia='" + contrasenia + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario usuario)) return false;
+        return id == usuario.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public abstract boolean login();
+    public abstract void logout();
+    public abstract boolean cambiarContrasenia();
+}
