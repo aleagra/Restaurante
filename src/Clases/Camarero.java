eab930d18359be7910381e695f3a75f405128217
Alejandro Agra <aleagra08@hotmail.com>
2024-11-15 18:13:17 -0300

    Add Class Camarero

---
+++ src/Clases/Camarero.java
@@ -0,0 +1,16 @@
+package Clases;
+
+public class Camarero {
+
+    public void registrarPedido(){
+
+    }
+
+    public void verMesasDisponibles(){
+
+    }
+
+    public void generarFactura(){
+
+    }
+}
