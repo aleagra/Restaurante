ebc65ab629b380f73b164923f458b61cb7165079
AvrilTraverso <avril.traverso9@gmail.com>
2024-11-15 18:03:36 -0300

    Add class Cliente

---
+++ src/Clases/Cliente.java
@@ -0,0 +1,68 @@
+package Clases;
+
+import java.util.List;
+
+public class Cliente extends Usuario{
+    private String telefono;
+    private String direccion;
+
+    public Cliente(String nombre, String apellido, String email, String contrasenia, String telefono, String direccion) {
+        super(nombre, apellido, email, contrasenia);
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + super.toString() +
+                "telefono='" + telefono + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean login() {
+        return false;
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public boolean cambiarContrasenia() {
+        return false;
+    }
+
+    public void hacerReserva(){
+
+    }
+
+    public void hacerPedido(){
+
+    }
+
+    public String consultarMenu(){
+        return null;
+    }
+
+
+
+}
