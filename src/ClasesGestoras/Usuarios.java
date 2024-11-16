0058b29efcbf18ab802e107878210a09292ffac8
Alejandro Agra <aleagra08@hotmail.com>
2024-11-16 15:07:53 -0300

    Add methods with Exceptions

--- src/ClasesGestoras/Usuarios.java
+++ src/ClasesGestoras/Usuarios.java
@@ -1,5 +1,9 @@
 package ClasesGestoras;
 import Clases.Usuario;
+import Excepciones.AutenticacionException;
+import Excepciones.EmailDuplicadoException;
+import Excepciones.UsuarioNoEncontradoException;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,24 +14,57 @@ public class Usuarios {
         this.listaUsuarios = new HashSet<>();
     }
 
-    public void registrarUsuario(){
-
+    public void registrarUsuario(Usuario usuario) throws EmailDuplicadoException {
+        if (!listaUsuarios.add(usuario)) {
+            throw new EmailDuplicadoException("El usuario con este email ya está registrado.");
+        }
+        System.out.println("Usuario registrado exitosamente.");
     }
 
-    public String loguearUsuario(){
-        return null;
+    public void loguearUsuario(String email, String contrasenia) throws AutenticacionException {
+        for (Usuario usuario : listaUsuarios) {
+            if (usuario.getEmail().equals(email) && usuario.getContrasenia().equals(contrasenia)) {
+                System.out.println("Usuario logueado exitosamente");
+                return;
+            }
+        }
+        throw new AutenticacionException("Email o contraseña incorrectos");
     }
 
-    public String eliminarUsuario(){
-        return null;
+
+    public String eliminarUsuario(String email) throws UsuarioNoEncontradoException {
+        for (Usuario usuario : listaUsuarios) {
+            if (usuario.getEmail().equals(email)) {
+                listaUsuarios.remove(usuario);
+                return "Usuario eliminado exitosamente.";
+            }
+        }
+        throw new UsuarioNoEncontradoException("El usuario con este email no existe.");
     }
 
-    public String mostrarUsuarios(){
-        return null;
+    public String mostrarUsuarios() {
+        if (listaUsuarios.isEmpty()) {
+            return "No hay usuarios registrados.";
+        }
+
+        StringBuilder resultado = new StringBuilder();
+        for (Usuario usuario : listaUsuarios) {
+            resultado.append(usuario.toString()).append("\n");
+        }
+
+        return resultado.toString();
     }
 
-    public String filtrarPorClase(){
-        return null;
+    public String filtrarPorClase(String nombreClase) {
+        StringBuilder resultado = new StringBuilder();
+
+        for (Usuario usuario : listaUsuarios) {
+            if (usuario.getClass().getSimpleName().equalsIgnoreCase(nombreClase)) {
+                resultado.append(usuario.toString()).append("\n");
+            }
+        }
+
+        return !resultado.isEmpty() ? resultado.toString() : "No se encontraron usuarios de la clase " + nombreClase;
     }
 }
 
