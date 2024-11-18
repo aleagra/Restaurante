f5a17c80dfe2cea4e4c22edfacaf4a34e21ecf24
TomassAlberti <alberti3113@gmail.com>
2024-11-18 16:55:38 -0300

    Registrar pedido Camarero()

--- src/Clases/Usuarios/Camarero.java
+++ src/Clases/Usuarios/Camarero.java
@@ -1,10 +1,11 @@
 package Clases.Usuarios;
 
+import Clases.Gestion.Pedido;
+import Enums.EstadoPedido;
+
 public class Camarero {
 
-    public void registrarPedido(){
-
-    }
+    public void registrarPedido(Pedido pedido) {pedido.actualizarEstadoPedido(EstadoPedido.PENDIENTE);}
 
     public void verMesasDisponibles(){
 
