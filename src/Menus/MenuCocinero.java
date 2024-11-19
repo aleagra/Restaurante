1f1049b00376867bff1a144770fd37de0e5b7bf9
Maria Sol Giuria <mariasolgiuria@gmail.com>
2024-11-19 16:34:14 -0300

    Menu Cocinero
    recibe la info con la que debe trabajar y se especifica el alcance de su trabajo en el sistema con las opciones del menu

---
+++ src/Menus/MenuCocinero.java
@@ -0,0 +1,58 @@
+package Menus;
+
+import Clases.Gestion.Pedido;
+import Clases.Usuarios.Cocinero;
+import ClasesGestoras.Pedidos;
+import Enums.EstadoPedido;
+
+import java.util.Scanner;
+
+public class MenuCocinero {
+
+    public void mostrarMenuCocinero(Pedidos pedidos, int nroPedido){
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        char seguir = 's';
+        Cocinero cocinera = new Cocinero("Sol","Giuria","solgiuria@gmail.com","aaa111");
+
+        do{
+            System.out.println("-----------------MENU---------------");
+            opciones();
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch(opcion){
+                case 1:
+                    System.out.println("Mostrando los pedidos...");
+                    cocinera.verPedido(pedidos);
+                    break;
+                case 2:
+                    System.out.println("Actualizando el estado del pedido...");
+                    cocinera.actualizarEstadoPedido(pedidos,nroPedido,EstadoPedido.LISTO);
+                    break;
+                case 3:
+                    System.out.println("Cambiando contrasenia...");
+                    cocinera.cambiarContrasenia("bbb2222");
+                    break;
+
+                default: {
+                    System.out.println("Número fuera de rango. Intenta de nuevo.");
+                    // No se usa break aca porque queremos que el do-while vuelva a ejecutarse.
+                    continue;
+                }
+            }
+            System.out.println("Desea continuar? s/n: ");
+            seguir = sc.next().charAt(0);
+
+        }while(seguir=='s');
+    }
+
+
+    public static void opciones(){
+        System.out.println("1-Ver pedidos");
+        System.out.println("2-Actualizar estado del pedido");
+        System.out.println("3- Cambiar contrasenia");
+        System.out.println("4- Salir...");
+    }
+
+}
