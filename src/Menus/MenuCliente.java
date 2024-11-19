d71e269d70c5d1d284b1d06799c50f9c6f6ecb92
Maria Sol Giuria <mariasolgiuria@gmail.com>
2024-11-19 19:05:13 -0300

    Cliente updated

--- src/Menus/MenuCliente.java
+++ src/Menus/MenuCliente.java
@@ -1,4 +1,47 @@
 package Menus;
+import Clases.Gestion.Mesa;
+import Clases.Gestion.Reserva;
+import Clases.Usuarios.Cliente;
+import ClasesGestoras.Carta;
+import ClasesGestoras.Mesas;
+import ClasesGestoras.Pedidos;
+import ClasesGestoras.Reservas;
+import java.util.Scanner;
 
 public class MenuCliente {
+
+    public void MostrarMenu(Scanner scanner,Mesas mesas,Cliente cliente,Carta carta,Reservas reservas,Pedidos pedidos){
+        int opcion;
+        do {
+            System.out.println("1- Hacer una reserva");
+            System.out.println("2- Ver reservas");
+            System.out.println("3- Ver pedidos");
+            System.out.println("4- Ver estado de un pedido");
+            System.out.println("4- Cerrar sesion");
+            opcion= scanner.nextInt();
+            scanner.nextLine();
+            switch (opcion){
+                case 1:
+                    Mesa mesa=new Mesa();
+                    System.out.println("Creando reserva...");
+                    System.out.println("Cuantas personas son? \n");
+                    opcion = scanner.nextInt();
+                    mesa=mesas.asignarMesa(opcion);
+                    Reserva reserva = new Reserva(mesa,cliente);
+                    reservas.aniadirReserva(reserva);
+                    break;
+                case 2:
+                    System.out.println(reservas.mostrarReservasPorCliente(cliente.getEmail()));
+                    break;
+                case 3:
+                    System.out.println(pedidos.mostrarPedidosPorCliente(cliente.getEmail()));
+                    break;
+                case 4:
+                    break;
+            }
+        }while (opcion != 5);
+
+
+    }
+
 }
