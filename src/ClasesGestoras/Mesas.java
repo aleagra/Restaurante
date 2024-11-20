152de8944b5b9de3c05f1a97c6086bd0f4fc26ed
Alejandro Agra <aleagra08@hotmail.com>
2024-11-19 22:05:05 -0300

    Revert "Interfaz Generica IMetodosGestores()"
    This reverts commit 8c2232fe9a18f0353dd3b1e0d44e088856959003.

--- src/ClasesGestoras/Mesas.java
+++ src/ClasesGestoras/Mesas.java
@@ -3,32 +3,45 @@ package ClasesGestoras;
 import Clases.Gestion.Mesa;
 import Enums.EstadoMesa;
 import Excepciones.MesasException;
-import Interfaces.IMetodosGestores;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class Mesas implements IMetodosGestores <Mesa>{
+public class Mesas {
     private ArrayList<Mesa> TodasLasMesas;
 
     public Mesas() {
         TodasLasMesas = new ArrayList<>();
     }
 
-    /**Cuando se lo llama hay que pasarle por parametro mesas.TodasLasMesas*/
-    @Override
-    public String agregarElemento(List<Mesa> lista, Mesa elemento) throws Exception {
-        return IMetodosGestores.super.agregarElemento(lista, elemento);
+
+
+    public String addMesa(Mesa m) throws MesasException {
+        String msj = "La mesa se aniadio con exito!";
+        if(m!= null){
+            TodasLasMesas.add(m);
+        }else{
+            throw new MesasException("El mesa no puede ser null");
+        }
+
+        return msj;
     }
 
-    @Override
-    public String eliminarElemento(List<Mesa> lista, Mesa elemento) throws Exception {
-        return IMetodosGestores.super.eliminarElemento(lista, elemento);
+    public String deleteMesa(Mesa m) throws MesasException {
+        String msj = "La mesa se eliminado con exito!";
+            if(TodasLasMesas.contains(m)){
+                TodasLasMesas.remove(m);
+            }else {
+                throw new MesasException("El mesa no existe");
+            }
+            return msj;
     }
 
-    @Override
-    public String mostrarElementos(List<Mesa> lista) {
-        return IMetodosGestores.super.mostrarElementos(lista);
+    public String mostrarMesas(){
+        StringBuilder builder = new StringBuilder();
+        for (Mesa m : TodasLasMesas) {
+            builder.append(m.toString());
+        }
+        return builder.toString();
     }
 
     public String mostrarMesasDisponibles(){
